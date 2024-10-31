d2be53207cf1bceed9d980268c97e59dd6efee48
dune123 <96974209+dune123@users.noreply.github.com>
2024-10-31 12:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
-}
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let result="";
 
+	let len=characters.length;
+	for(let i=0;i<l;i++){
+		result+=characters.charAt(Math.floor(Math.random() * len));
+	}
+	return result;
+}
+ 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
